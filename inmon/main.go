166e66411945bf9182593ac4c02d7ed27003b2d1
Jason Olson <jason.olson@opensesame.com>
2026-05-02 14:52:39 -0700

    inmon: create input monitor to look at byte codes of key combinations

--- inmon/main.go
+++ inmon/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/term"
+)
 
 func main() {
-	fmt.Println("Hello from inmon!")
+	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
+	if err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	defer func() {
+		_ = term.Restore(int(os.Stdin.Fd()), oldState)
+	}()
+	fmt.Println("Press Ctrl+C to quit\r")
+
+	var buf [1]byte
+	for {
+		n, err := os.Stdin.Read(buf[:])
+		if err != nil || n == 0 {
+			break
+		}
+		b := buf[0]
+		if b == 0x03 { // Ctrl+C
+			break
+		}
+		if b >= 32 && b <= 126 {
+			fmt.Printf("0x%02X  %q\r\n", b, b)
+		} else {
+			fmt.Printf("0x%02X\r\n", b)
+		}
+	}
 }
